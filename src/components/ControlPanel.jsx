360efec1f34a63706cf8f7fdaff959275ca6c918
Codemozart <thecodemozart@gmail.com>
2026-09-08 05:04:12 +0200

    make UI collapsable

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import { useTerrainStore } from "../store/useTerrainStore";
 
 export default function ControlPanel() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   const maxHeight = useTerrainStore((s) => s.maxHeight);
   const setMaxHeight = useTerrainStore((s) => s.setMaxHeight);
   const resolution = useTerrainStore((s) => s.resolution);
   const setResolution = useTerrainStore((s) => s.setResolution);
   const uploadTexture = useTerrainStore((s) => s.uploadTexture);
   const reloadShader = useTerrainStore((s) => s.reloadShader);
+  const debugMode = useTerrainStore((s) => s.debugMode);
+  const setDebugMode = useTerrainStore((s) => s.setDebugMode);
 
-  return (
-    <div className="control-panel">
+  const handlePanelClick = (e) => {
+    // Ignore clicks if they happened on or inside an input, select, button, or label
+    if (e.target.closest("input, select, button, label")) {
+      return;
+    }
+    setIsCollapsed((prev) => !prev);
+  };
+
+  const inPanel = (html) => {
+    return (
+      <div className="control-panel" onClick={handlePanelClick}>
+        {html}
+      </div>
+    );
+  };
+
+  if (isCollapsed) {
+    return inPanel(<p>click to expand</p>);
+  }
+
+  return inPanel(
+    <>
       <label>
         Heightmap Image:
         <input
@@ -43,7 +68,19 @@ export default function ControlPanel() {
         />
       </label>
 
+      <label>
+        Display Mode:
+        <select
+          value={debugMode}
+          onChange={(e) => setDebugMode(parseInt(e.target.value))}
+        >
+          <option value={0}>Shaded Terrain</option>
+          <option value={1}>Elevation Map</option>
+          <option value={2}>Normal Vectors</option>
+        </select>
+      </label>
+
       <button onClick={reloadShader}>Reload Shader</button>
-    </div>
+    </>,
   );
 }
